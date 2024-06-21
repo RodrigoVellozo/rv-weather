6f330e84397d2d1a60be5c70878f07f2fb1eaec6
Rodrigo Vellozo <rodrigo.vellozo@rwetelemedicina.com.br>
2024-06-21 13:44:54 -0300

    iniciar desenvolvimento da UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonBadge,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonTitle,
+  IonToolbar,
+  IonCard
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { menuOutline, notificationsOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonBadge,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonIcon,
+    IonBadge,
+    IonCard
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({menuOutline, notificationsOutline});
+  }
 }
